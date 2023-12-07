efbaa7b872f2f229cee8e1426f1d8f6d8ee94721
GroggyGosling <bonnedijk@ziggo.nl>
2023-12-07 13:09:22 +0100

    added fit  to subtracted data

--- src/nsp2/fitten_rechte_lijn.py
+++ src/nsp2/fitten_rechte_lijn.py
@@ -29,7 +29,7 @@ class FitLinearData():
 
 class FitGaussianData():
 
-    def __init__(self):
+    def __init__(self, subtract):
         self.file_NA22 = open('data\calibratie\spectrum_NA22.csv', 'r')
         self.file_CS137 = open('data\calibratie\spectrum_CS137.csv', 'r')
         self.file_background = open('data\calibratie\spectrum_background.csv', 'r')
@@ -61,11 +61,21 @@ class FitGaussianData():
         self.maximum_CS137_2_pulseheight = []
         self.maximum_NA22_1_pulseheight = []
         self.maximum_NA22_2_pulseheight = [] 
+        self.subtracted = subtract
+        self.subtract_max = []
+        self.subtract_x = []
 
         self.fit_c = []
         self.fit_c_error = []
 
     def select_maximum(self):
+        i = 0
+        for x in self.pulseheight:
+            if x >= 90 and x <= 180:
+                self.subtract_max.append(float(self.subtracted[i]))
+                self.subtract_x.append(float(self.pulseheight[i]))
+            i += 1
+
         i = 0
         for x in self.pulseheight:
             if x >= 120 and x <= 150:
@@ -96,40 +106,47 @@ class FitGaussianData():
     def gaussian_fit(self):
         model = Model(self.gaussian_func)
 
-        self.result_NA22_1 = model.fit(self.maximum_NA22_1, x = self.maximum_NA22_1_pulseheight, a = 20 , b = 1150, c = 136, sigma = 1)
-        self.fit_c.append(self.result_NA22_1.params['c'].value)
-        self.fit_c_error.append(self.result_NA22_1.params['c'].stderr/np.sqrt(np.sum(self.maximum_NA22_1)))
+        self.result_subtract = model.fit(self.subtract_max, x = self.subtract_x, a = 2, b = 100, c = 136, sigma = 1)
+        print(self.result_subtract.fit_report())
+        # self.result_NA22_1 = model.fit(self.maximum_NA22_1, x = self.maximum_NA22_1_pulseheight, a = 20 , b = 1150, c = 136, sigma = 1)
+        # self.fit_c.append(self.result_NA22_1.params['c'].value)
+        # self.fit_c_error.append(self.result_NA22_1.params['c'].stderr/np.sqrt(np.sum(self.maximum_NA22_1)))
 
-        self.result_NA22_2 = model.fit(self.maximum_NA22_2, x = self.maximum_NA22_2_pulseheight, a = 1 , b = 140, c = 328, sigma = 1)
-        self.fit_c.append(self.result_NA22_2.params['c'].value)
-        self.fit_c_error.append(self.result_NA22_2.params['c'].stderr/np.sqrt(np.sum(self.maximum_NA22_2)))
+        # self.result_NA22_2 = model.fit(self.maximum_NA22_2, x = self.maximum_NA22_2_pulseheight, a = 1 , b = 140, c = 328, sigma = 1)
+        # self.fit_c.append(self.result_NA22_2.params['c'].value)
+        # self.fit_c_error.append(self.result_NA22_2.params['c'].stderr/np.sqrt(np.sum(self.maximum_NA22_2)))
 
-        self.result_CS137_1 = model.fit(self.maximum_CS137_1, x = self.maximum_CS137_1_pulseheight, a = 10 , b = 900, c = 55, sigma = 1)
-        self.fit_c.append(self.result_CS137_1.params['c'].value)
-        self.fit_c_error.append(self.result_CS137_1.params['c'].stderr/np.sqrt(np.sum(self.maximum_CS137_1)))
+        # self.result_CS137_1 = model.fit(self.maximum_CS137_1, x = self.maximum_CS137_1_pulseheight, a = 10 , b = 900, c = 55, sigma = 1)
+        # self.fit_c.append(self.result_CS137_1.params['c'].value)
+        # self.fit_c_error.append(self.result_CS137_1.params['c'].stderr/np.sqrt(np.sum(self.maximum_CS137_1)))
 
-        self.result_CS137_2 = model.fit(self.maximum_CS137_2, x = self.maximum_CS137_2_pulseheight, a = 10 , b = 2250, c = 170, sigma = 1)
-        self.fit_c.append(self.result_CS137_2.params['c'].value)
-        self.fit_c_error.append(self.result_CS137_2.params['c'].stderr/np.sqrt(np.sum(self.maximum_CS137_2)))
+        # self.result_CS137_2 = model.fit(self.maximum_CS137_2, x = self.maximum_CS137_2_pulseheight, a = 10 , b = 2250, c = 170, sigma = 1)
+        # self.fit_c.append(self.result_CS137_2.params['c'].value)
+        # self.fit_c_error.append(self.result_CS137_2.params['c'].stderr/np.sqrt(np.sum(self.maximum_CS137_2)))
 
-        print(self.result_NA22_1.fit_report())
-        print(self.result_NA22_2.fit_report())
-        print(self.result_CS137_1.fit_report())
-        print(self.result_CS137_2.fit_report())
+        # print(self.result_NA22_1.fit_report())
+        # print(self.result_NA22_2.fit_report())
+        # print(self.result_CS137_1.fit_report())
+        # print(self.result_CS137_2.fit_report())
     
-        return self.fit_c, self.fit_c_error
+        # return self.fit_c, self.fit_c_error
     
     def plot_data(self):
-        plt.plot(self.pulseheight, self.counts_NA22)
-        plt.show()
-        plt.plot(self.pulseheight, self.counts_CS137)
-        plt.show()
-        plt.plot(self.pulseheight, self.counts_background)
+        # plt.plot(self.pulseheight, self.counts_NA22)
+        # plt.show()
+        # plt.plot(self.pulseheight, self.counts_CS137)
+        # plt.show()
+        # plt.plot(self.pulseheight, self.counts_background)
+        # plt.show()
+        plt.plot(self.subtract_x, self.subtract_max)
+        plt.plot(self.subtract_x, self.result_subtract.best_fit)
         plt.show()
 
 class compton():
 
     def __init__(self):
+        # self.file_background_20 = open('data\\04_12\\background_20m_45d_4_12.csv', 'r')
+        # self.file_1h = open('data\\04_12\\1h_45d_4_12.csv', 'r')
         self.file_background_20 = open('data\\30_11\\background_20min_30_11.csv', 'r')
         self.file_1h = open('data\\30_11\\1h_30_11.csv', 'r')
 
@@ -147,6 +164,11 @@ class compton():
         for row in csvreader:
             self.counts_background_20.append(3 * int(float(row[1])))
 
+    def smoothing(self):
+        self.x = np.array(self.pulseheight)
+        self.y = savitzky_golay(np.array(self.counts_background_20),window_size= 51, order=10) 
+        
+
     def approx_background(self):
         self.background = []
         for i in self.pulseheight:
@@ -154,10 +176,12 @@ class compton():
 
     def subtract(self): 
         self.subtracted = []
-        for i,j in zip(self.counts_1h, self.counts_background_20):
+        for i,j in zip(self.counts_1h, self.y):
             self.subtracted.append(i - j)
+        return self.subtracted
         
     def plot(self):
+        plt.plot(self.x, self.y)
         plt.plot(self.pulseheight, self.counts_1h, label = 'met object')
         plt.plot(self.pulseheight, self.counts_background_20, label = 'zonder object')
         plt.plot(self.pulseheight, self.subtracted, label = 'subtracted')
@@ -216,17 +240,17 @@ class lineair_achtergond():
         plt.plot(self.pulseheight, self.counts)
         plt.show()
 
-    def smoothing(self):
-        x = np.array([self.achtergrond_pulseheight])
-        y = savitzky_golay(np.array([self.achtergrond_counts]),window_size= 50, order=3) 
-        plt.plot(x,y)
-        plt.show()
+    
 
+comptontest = compton()
+comptontest.smoothing()
+subtract = comptontest.subtract()
+comptontest.plot()
 
-
-test = FitGaussianData()
-test = compton()
-test = lineair_achtergond()
+fit = FitGaussianData(subtract)
+fit.select_maximum()
+fit.gaussian_fit()
+fit.plot_data()
 
 
 
